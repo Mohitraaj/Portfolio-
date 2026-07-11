b10b01f6578fa3ce467fdcc1bba18df0b8540baa
Mohitraaj <5w4hs2j7cy@privaterelay.appleid.com>
2026-07-11 08:38:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,23 @@ nav.style.boxShadow="none";
 }
 
 });
+// Reveal Animation
+
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+}
+});
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+section.style.opacity="0";
+section.style.transform="translateY(60px)";
+section.style.transition="1s";
+
+observer.observe(section);
+
+});
